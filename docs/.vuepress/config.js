76bd7bc8cd264b58c6466704bc58c974792e0dc1
wassan128 <wassan.taiko.master@gmail.com>
2020-08-01 17:58:05 +0900

    Add docs and config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+const { description } = require('../../package')
+
+module.exports = {
+  title: 'ペンギンの道標',
+  description: 'wassan128の技術だったり技術じゃなかったりするブログです',
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#3297bf' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  themeConfig: {
+    base: '/weblog/',
+    logo: 'https://pbs.twimg.com/profile_images/1245187472155389952/M_TAmpP5_400x400.jpg',
+    repo: 'wassan128/weblog',
+    editLinks: false,
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+    sidebar: 'auto',
+    sidebarDepth: 6,
+  },
+
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
+
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: https://1.bp.blogspot.com/-SgT2G_vDGwE/XQjt4RWH1TI/AAAAAAABTNc/0He0eUi8-7QAd0RDvxWGA1MBzphu9hvsgCLcBGAs/s450/animal_chara_computer_penguin.png
+heroText: ペンギンの道標
+tagline: 技術だったり技術じゃなかったりするブログ
+footer: © 2020-present wassan128
+---
